4e1c3f16b8c4648cefb759f273e36756cc0b8de4
Halid Ozkilic <halit1as@gmail.com>
2020-11-23 11:14:11 +0300

    configurations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Dashboard1Component } from './dashboard1/dashboard1/dashboard1.component';
+import { Dashboard2Component } from './dashboard2/dashboard2/dashboard2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'dashboard', pathMatch:'full'},
+  {path:'dashboard1', component:Dashboard1Component},
+  {path:'dashboard2', component:Dashboard2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatSliderModule } from '@angular/material/slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Dashboard1Component } from './dashboard1/dashboard1/dashboard1.component';
 import { Dashboard2Component } from './dashboard2/dashboard2/dashboard2.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Dashboard1Component,
-    Dashboard2Component
+    Dashboard2Component,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
