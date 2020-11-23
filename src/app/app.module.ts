dc9e1f7cdef76f250b1b6770a3b579ef31168e60
Halid Ozkilic <halit1as@gmail.com>
2020-11-23 09:59:33 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Dashboard1Component } from './dashboard1/dashboard1/dashboard1.component';
+import { Dashboard2Component } from './dashboard2/dashboard2/dashboard2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Dashboard1Component,
+    Dashboard2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
